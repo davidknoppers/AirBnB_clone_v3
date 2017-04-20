df3e9acbd05c7f68f980d17e3069cf10848343f5
davidknoppers <david.knoppers@holbertonschool.com>
2017-04-20 18:04:19 +0000

    updating stats method

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+Basic setup to return count of each class in db
 """
 from api.v1.views import app_views
 from flask import jsonify
@@ -10,6 +11,19 @@ def status():
     return jsonify({'status': "Ok"})
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', methods=['GET'])
 def stats():
-    return "storage.get"
+    """
+    from models import storage
+    obj_counts = {}
+    classes = self.__models_available.keys()
+    for cls in classes:
+        obj_counts[cls] = self.count(cls)
+    return obj_counts
+    """
+    from models import storage
+    models_available = ["User", "Amenity", "City", "Place", "Review", "State"]
+    output = {}
+    for model in models_available:
+        output[model] = storage.count(model)
+    return jsonify(output)
