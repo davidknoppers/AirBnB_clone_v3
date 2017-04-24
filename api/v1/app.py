bbe2280e68a597606e232962368db8daf178999a
guptaNswati <sgupta1505@gmail.com>
2017-04-24 22:26:03 +0000

    v1 14

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,11 +4,13 @@ This module starts a rest api.
 """
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS, cross_origin
 from models import storage
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
