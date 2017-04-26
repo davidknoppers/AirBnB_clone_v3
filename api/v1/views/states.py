1f881315152c76b0c45e277040e2b652e41e1c2a
davidknoppers <david.knoppers@holbertonschool.com>
2017-04-26 00:05:50 +0000

    scrounging for checks

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -21,24 +21,14 @@ def get_all_states():
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_a_state(state_id=None):
     """ Retrieves a State object based on given id """
-<<<<<<< HEAD
     if state_id is None:
         abort(404)
     state = storage.get("State", state_id)
     if state is None:
-=======
-    if not state_id:
-        abort(404)
-    try:
-        state = storage.get("State", state_id)
-        return jsonify(state.to_json())
-    except:
->>>>>>> 66f0b97b2d459a9f08989de97159107f3284a2cf
         abort(404)
     return jsonify(state.to_json())
 
 
-<<<<<<< HEAD
 @app_views.route('/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
 def del_state(state_id=None):
@@ -48,31 +38,16 @@ def del_state(state_id=None):
     state = storage.get("State", state_id)
     if state is None:
         abort(404)
-=======
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
-def del_state(state_id):
-    """ Deletes a State object """
-    if not state_id:
-        abort(404)
-    state = storage.get("State", state_id)
->>>>>>> 66f0b97b2d459a9f08989de97159107f3284a2cf
     storage.delete(state)
     return jsonify({}), 200
-
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """ Creates a State """
     content = request.get_json()
-<<<<<<< HEAD
     if content is None:
         return "Not a JSON", 400
     if 'name' not in content.keys():
-=======
-    if not content:
-        return "Not a JSON", 400
-    if "name" not in content.keys():
->>>>>>> 66f0b97b2d459a9f08989de97159107f3284a2cf
         return "Missing name", 400
     state = State(**content)
     state.save()
@@ -80,16 +55,8 @@ def create_state():
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-<<<<<<< HEAD
 def update_state(state_id=None):
-    """updates a state, if it exists"""
-=======
-def update_state(state_id):
-    """ Updates a State object """
-    state = storage.get("State", state_id).to_json()
-    if state is None:
-        abort(404)
->>>>>>> 66f0b97b2d459a9f08989de97159107f3284a2cf
+    """updates a state object"""
     try:
         r = request.get_json()
     except:
